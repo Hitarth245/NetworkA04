d67db13598bb5c0431f492026f3af263a2300cca
Hitarth24 <patelhitarth4000@gmail.com>
2024-04-06 21:03:39 -0400

    Deleting the Posts and the photos to the posts are bieng added

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -31,7 +31,12 @@ const getCookie =(name) => {
     return cookieValue;
 }
 const csrftoken = getCookie('csrftoken');
-console.log(csrftoken)
+
+const deleted = localStorage.getItem('title')
+if(deleted){
+    handleAlerts('danger', `deleted "${deleted}"`)
+    localStorage.clear()
+}
 
 const likeUnlikePosts = ()=> {
     const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
